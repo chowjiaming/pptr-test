0566f37e1410b0b360b61c3e9df0af5a4045d583
Joseph Chow <chowjiaming@pop-os.localdomain>
2022-08-08 16:48:34 -0400

    added inspection summary

--- src/components/InspectionSummary.tsx
+++ src/components/InspectionSummary.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import {HeadingBar} from './shared/HeadingBar';
+import {MarkupImagePlaceholder} from '../styles/helpers';
 
 export function InspectionSummary(): JSX.Element {
   return (
     <>
       <h1>InspectionSummary</h1>
+      <HeadingBar />
+      <MarkupImagePlaceholder />
+      <h2 className="small-upper grey">
+        User Summary Input &#40;Max. Word&#41;
+      </h2>
     </>
   );
 }
